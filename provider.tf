335df1f7defb72801b78a42c7f90fae9ee58fd29
mandu1234567 <mandeeppathania333@gmail.com>
2026-04-07 14:18:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  
+  region = "us-east-1"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">4.0" 
-    }
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyInstance"
   }
 }
